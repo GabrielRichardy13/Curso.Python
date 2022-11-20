66e52d6a1c235526c7276b33442034ca19a6a272
Gabriel_Richardy <61621799+GabrielRichardy13@users.noreply.github.com>
2022-11-20 00:03:16 -0300

    Add files via upload

---
+++ #002.py
@@ -0,0 +1,9 @@
+#Respondendo ao Usuário
+
+'''Faça um programa que leia 
+o nome de uma pessoa e mostre 
+uma mensagem de boas-vindas.'''
+
+print ('Olá, seja bem-vindo!')
+nome = input('Digite o seu nome: ')
+print (f'Muito prazer {nome}!')
