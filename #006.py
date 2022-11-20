1e6364816d67de7bfc5744c001887a1182142d1e
Gabriel_Richardy <61621799+GabrielRichardy13@users.noreply.github.com>
2022-11-20 00:04:32 -0300

    Add files via upload

---
+++ #006.py
@@ -0,0 +1,8 @@
+#Dobro, Triplo, Raiz Quadrada
+
+''' Crie um algoritmo que leia 
+um número e mostre o seu dobro, 
+triplo e raiz quadrada.'''
+
+n = int(input('Digite um número: '))
+print (f'O dobro de {n} é {n * 2}, o triplo é {n*3} e a raiz quadrada é {n ** (1/2):.2f}.')
