29e2cf99a89c227824e992566c46ebd3f08afc03
Gabriel_Richardy <61621799+GabrielRichardy13@users.noreply.github.com>
2022-11-20 00:03:54 -0300

    Add files via upload

---
+++ #003.py
@@ -0,0 +1,9 @@
+#Somando dois números
+
+'''Crie um programa que leia 
+dois números e mostre a soma 
+entre eles.'''
+
+n1 = int(input('Digite um número: '))
+n2 = int(input('Digite outro número: '))
+print (f'A soma de {n1} + {n2} é igual a {n1 + n2}')
