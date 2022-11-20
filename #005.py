f2b8c5e4cd72960ef447c999d0bd6d389ecdd1e1
Gabriel_Richardy <61621799+GabrielRichardy13@users.noreply.github.com>
2022-11-20 00:04:20 -0300

    Add files via upload

---
+++ #005.py
@@ -0,0 +1,8 @@
+#Predecessor e Sucessor
+
+''' Faça um programa que 
+lê um inteiro e mostre na 
+tela seu sucessor e antecessor.'''
+
+n = int(input('Digite um número: '))
+print (f'O número {n} tem como sucessor {n + 1} e antecessor {n - 1}')
