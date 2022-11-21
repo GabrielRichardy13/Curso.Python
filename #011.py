db5eeb881f5003755b25c45549b99aa18647a3e1
Gabriel_Richardy <61621799+GabrielRichardy13@users.noreply.github.com>
2022-11-21 20:20:56 -0300

    Add files via upload

---
+++ #011.py
@@ -0,0 +1,5 @@
+lp = float(input('Digite a largura da parede: '))
+ap = float(input('Digite a altura da parede: '))
+
+print (f'Sua parede tem a dimenção de {lp}X{ap} e sua área é de {lp*ap}m².\n'
+       f'Para pintar essa parede, você precisará de {(lp*ap)/2}L de tinta.')
