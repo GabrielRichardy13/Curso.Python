0e69816f67c8100b192da4f32b37c119f2bebc1d
Gabriel_Richardy <61621799+GabrielRichardy13@users.noreply.github.com>
2022-11-22 23:08:24 -0300

    Add files via upload

---
+++ #012.py
@@ -0,0 +1,8 @@
+#Calculando Descontos
+
+'''Faça um algoritmo que leia o preço 
+de um produto e mostre seu novo preço, 
+com 5% de desconto.'''
+
+n = float(input('Digite um valor: R$'))
+print (f'O valor de R${n:.2f} com 5% d0e desconto é R${n * 5/ 100:.2f}')
