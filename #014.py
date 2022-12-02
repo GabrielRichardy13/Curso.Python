6e88e5136f239e9b5cc39dab23127404258f762e
Gabriel_Richardy <61621799+GabrielRichardy13@users.noreply.github.com>
2022-12-02 17:07:28 -0300

    Add files via upload

---
+++ #014.py
@@ -0,0 +1,8 @@
+#Conversor de Temperaturas
+
+'''Escreva um programa que converta 
+uma temperatura digitando em graus 
+Celsius e converta para graus Fahrenheit.'''
+
+tc = float(input('Digite a temperatura em ºC: '))
+print (f'A temperatura {tc}ºC é igual á {9 * tc / 5 + 32:.1f}ºF')
