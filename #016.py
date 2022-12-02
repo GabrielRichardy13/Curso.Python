64aa96a3bb6ee8a557c6bc474aba0ca0609651e9
Gabriel_Richardy <61621799+GabrielRichardy13@users.noreply.github.com>
2022-12-02 17:08:05 -0300

    Add files via upload

---
+++ #016.py
@@ -0,0 +1,8 @@
+#Quebrando um número
+
+'''Crie um programa que leia um 
+número Real qualquer pelo teclado e 
+mostre na tela a sua porção Inteira.'''
+
+num = float(input('Digite um número: '))
+print (f'O número {num} tem como sua porção inteira {int(num)}')
